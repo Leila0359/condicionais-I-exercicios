43fe600a3cdcad5ca461e04656a5bd54a66797d6
leila0359 <leilateixeira1985@gmail.com>
2023-09-12 23:40:13 -0300

    exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+function verificarPar(numero) {
+
+    if (numero * 10  === 0) {
+      console.log('qual numero par')
+       return "o numero e par."
+    } else { 
+    return "o numero nao e par." 
+    }  
+} 
+const numero = 10
+console.log(verificarPar(numero)) 
+
+    
+    
+    
+
+
+
+
+
